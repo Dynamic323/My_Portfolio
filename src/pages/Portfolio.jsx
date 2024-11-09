35f38bad93424ceb421033286ff042d2547aaff1
DycoderX <152976701+Dynamic323@users.noreply.github.com>
2024-11-09 17:31:20 +0100

    Update Portfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
-import Pagebg from "../components/pagebg";
+import Pagebg from "../components/Pagebg";
 import { images } from "../assets";
 
 function Portfolio() {
