a35ef04ddeb8bc319c12cde57bf4daecb92bb3b1
John Doe <johndoe@example.com>
2024-11-09 17:36:49 +0100

    edited contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -30,11 +30,14 @@ function Contact() {
         <section className="mapbox" data-mapbox>
           <figure>
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d199666.5651251294!2d-121.58334177520186!3d38.56165006739519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809ac672b28397f9%3A0x921f6aaa74197fdb!2sSacramento%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1647608789441!5m2!1sen!2sbd"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3232.138540140844!2d5.638440073936543!3d6.354244793635705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1040d360d5f4e017%3A0x39e60adf8645b6f0!2s9%20Edokpolor%20St%2C%20Use%2C%20Benin%20City%20300102%2C%20Edo!5e1!3m2!1sen!2sng!4v1731170131749!5m2!1sen!2sng"
               width="400"
               height="300"
               loading="lazy"
               title="Google Map"
+              style={{ border: "0" }}
+              allowfullscreen=""
+              referrerpolicy="no-referrer-when-downgrade"
             ></iframe>
           </figure>
         </section>
