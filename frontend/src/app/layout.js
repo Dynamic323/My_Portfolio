0c0f5459ea6cb6c7895b79bdf5614809ff4c3778
Dycoder <otamereisrael8@gmail.com>
2025-12-25 21:29:06 +0100

    _A

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,3 @@
-
 import "./globals.css";
 import "./App.css";
 import ParticleBackground from "../ParticleBackground";
@@ -6,29 +5,42 @@ import Layout from "../components/Layout";
 import Script from "next/script";
 
 export const metadata = {
-    title: "Dys-Portfolio",
-    description: "Portfolio of Dy",
+  title: "Israel Otamere (Dy Coder)",
+  description: "Full-stack developer portfolio",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <head>
-                <link rel="shortcut icon" href="/avarta_profile.png" type="image/x-icon" />
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
-                />
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/ionicons@5.5.2/dist/css/ionicons.min.css" />
-            </head>
-            <body>
-                <ParticleBackground />
-                <Layout>
-                    {children}
-                </Layout>
-                <Script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js" strategy="lazyOnload" />
-                <Script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js" strategy="lazyOnload" />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="shortcut icon"
+          href="/avarta_profile.png"
+          type="image/x-icon"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/ionicons@5.5.2/dist/css/ionicons.min.css"
+        />
+      </head>
+      <body>
+        <ParticleBackground />
+        <Layout>{children}</Layout>
+        <Script
+          type="module"
+          src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+          strategy="lazyOnload"
+        />
+        <Script
+          noModule
+          src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+          strategy="lazyOnload"
+        />
+      </body>
+    </html>
+  );
 }
